8e7defb7e49ee40b9af57fd65d8f63824436e1f5
Frank <jong1722@student.nhl.nl>
2019-02-08 12:19:00 +0100

    Menubar added

--- src/draweditor/frames/MainDrawFrame.java
+++ src/draweditor/frames/MainDrawFrame.java
@@ -1,15 +1,20 @@
 package draweditor.frames;
 
 import javax.swing.JFrame;
+
+import draweditor.components.MenuBar;
 	
 public class MainDrawFrame extends JFrame {
     private static String TITLE = "DrawEditor";
+    MenuBar makeMenu;
 
     public MainDrawFrame() {
         super();
         setTitle(TITLE);
         setSize(300,200); // default size is 0,0
         setLocation(10,200); // default is 0,0 (top left corner)
-    }
 
+        makeMenu = new MenuBar();
+        setJMenuBar(makeMenu);
+    }
 }
