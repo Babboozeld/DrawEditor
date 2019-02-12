fb1937ebb60b2e372c11634d6f3bf31ea7658b39
Frank <jong1722@student.nhl.nl>
2019-02-12 14:10:11 +0100

    Colorbar

--- src/draweditor/frames/MainDrawFrame.java
+++ src/draweditor/frames/MainDrawFrame.java
@@ -1,7 +1,9 @@
 package draweditor.frames;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
+import draweditor.components.ColorPicker;
 import draweditor.components.MenuBar;
 	
 public class MainDrawFrame extends JFrame {
@@ -14,6 +16,12 @@ public class MainDrawFrame extends JFrame {
         setSize(300,200); // default size is 0,0
         setLocation(10,200); // default is 0,0 (top left corner)
 
+        JComponent newContentPane = new ColorPicker();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        getContentPane().add(newContentPane, BorderLayout.EAST);
+        getContentPane().setLayout(new BorderLayout());
+        setContentPane(newContentPane);
+        
         makeMenu = new MenuBar();
         setJMenuBar(makeMenu);
     }
